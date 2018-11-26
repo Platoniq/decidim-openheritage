3457cfa866f14f89fdc86a1897026d63eee3bf06
Ivan Vergés <ivan@platoniq.net>
2018-11-26 18:35:02 +0100

    Added locales

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -6,7 +6,7 @@ Decidim.configure do |config|
 
   # Change these lines to set your preferred locales
   config.default_locale = :en
-  config.available_locales = [:en, :ca, :es]
+  config.available_locales = [:en, :ca, :es, :fr, :pt, :de, :fi, :nl, :eu, :ca, :gl, :hu, :it, :pl, :sv]
 
   # Geocoder configuration
   config.geocoder = {
