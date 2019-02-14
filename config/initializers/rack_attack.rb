ff2b866b5eca123327be3ad7e57b27545e6ee702
Ivan Vergés <ivan@platoniq.net>
2019-02-14 13:02:34 +0100

    Add matomo stats

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,17 @@
+if Rails.env.production?
+  Decidim.configure do |config|
+
+	# Max requests in a time period to prevent DoS attacks. Only applied on production.
+	config.throttling_max_requests = 100
+
+	# Time window in which the throttling is applied.
+	config.throttling_period = 1.minute
+
+  end
+
+	# Provided that trusted users use an HTTP request header named APIKey
+	Rack::Attack.safelist("mark any authenticated access safe") do |request|
+	  # Requests are allowed if the return value is truthy
+	  request.env["APIKey"] == "let-me-hack"
+	end
+end
