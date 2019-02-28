ee8c8e191427ecc1db7d7f27f63c908aa1ac215c
Ivan Vergés <ivan@platoniq.net>
2019-02-28 01:06:10 +0100

    add oauth/me to non ssl path

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
 #initializers/sidekiq.rb
-schedule_file = "config/schedule.yml"
 
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+if Rails.env.production?
+
+  schedule_file = "config/schedule.yml"
+
+  if File.exist?(schedule_file) && Sidekiq.server?
+    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  end
 end
