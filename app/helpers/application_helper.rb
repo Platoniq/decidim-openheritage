a6dcb0133d286414c8915a02a85d42dff1ccca12
Ivan Vergés <ivan@platoniq.net>
2019-07-17 13:43:02 +0200

    Matomo ID per tenant configurable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def get_matomo_id
+		return unless matomo = Rails.application.secrets.matomo
+		id = matomo[:tenants][current_organization&.host.to_sym] if matomo[:tenants].present?
+		id || matomo[:id]
+	end
 end
