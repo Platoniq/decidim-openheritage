39e8c15b9db473fc70d16ccdce2bc90f7ab4bd38
Ivan Vergés <ivan@platoniq.net>
2019-09-18 20:05:39 +0200

    add images to proposals cells

--- config/initializers/custom_overrides.rb
+++ config/initializers/custom_overrides.rb
@@ -1,1 +1,17 @@
 # frozen_string_literal: true
+# Add images to proposals cars
+#
+
+Rails.application.config.to_prepare do
+
+  #hide buttons if no next
+  Decidim::Proposals::ProposalMCell.class_eval do
+    def has_image?
+      model.photos.any?
+    end
+
+    def resource_image_path
+      model.photos.first.thumbnail_url
+    end
+  end
+end
