45499653dc19bcaa58128afa9452d9549fe54de7
Ivan Vergés <ivan@platoniq.net>
2020-02-26 13:42:53 +0100

    detect specific survey

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,13 @@ module ApplicationHelper
 		return false unless oh = Rails.application.secrets.openheritage
 		oh.include? current_organization&.host.to_str
 	end
+
+	def timetracker_survey
+		return unless timetracker = Rails.application.secrets.timetracker
+		return unless timetracker[:components].respond_to? :include?
+		return unless defined? current_component
+		return unless timetracker[:components].include? current_component&.id
+
+		Decidim::Surveys::Survey.find_by(component: current_component)
+	end
 end
