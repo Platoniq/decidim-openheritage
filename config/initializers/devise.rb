8daf31fe2d91a173d6c1ccd956e789d2e1d35ea8
Vera Rojman <vrojman@protonmail.com>
2021-06-14 15:02:43 +0200

    Fix rubocop offenses

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,4 +3,4 @@ Devise.setup do |config|
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
   config.timeout_in = 2.hours
-end 
+end
