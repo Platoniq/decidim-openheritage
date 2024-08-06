ee86833933f03c7021f0be18ac9aa9294eca9055
Harrison Hassig <harrisonhassig@gmail.com>
2024-08-06 16:08:28 +0200

    Fix rspec require in factories

--- spec/system/conferences_with_image_spec.rb
+++ spec/system/conferences_with_image_spec.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "decidim/conferences/test/factories"
 
-describe "conference with images", type: :system do
+describe "Conference with images", type: :system, perform_enqueued: true do
   let!(:organization) { create :organization }
   let!(:conference) { create :conference, organization: organization, slug: slug, registrations_enabled: true }
   let(:slug) { "bla-bla" }
@@ -11,10 +12,10 @@ describe "conference with images", type: :system do
   before do
     visit_component
     page.execute_script("$('#dc-modal-accept').click()")
-    visit Decidim::ResourceLocatorPresenter.new(post).path
+    page.visit "/conferences/bla-bla"
   end
 
   it "shows the image" do
-    expect(page).to have_selector("img src=")
+    expect(page).to have_selector("img")
   end
 end
