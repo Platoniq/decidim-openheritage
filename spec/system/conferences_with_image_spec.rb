5b77739fae5e263ccbbf2f6ea987d15c21847fd2
Harrison Hassig <harrisonhassig@gmail.com>
2024-08-08 13:58:33 +0200

    :conferences as manifest_name

--- spec/system/conferences_with_image_spec.rb
+++ spec/system/conferences_with_image_spec.rb
@@ -5,11 +5,11 @@ require "decidim/conferences/test/factories"
 
 describe "Conference with images", type: :system do
   include_context "with a component"
-  let(:manifest_name) { "conferences" }
-  let!(:organization) { create :organization }
+  let(:organization) { create :organization }
+  let(:manifest_name) { :conferences }
   let(:slug) { "bla-bla" }
   let(:short_description) { { en: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><img src=\"img.png\">" } }
-  let!(:conference) { create :conference, organization: organization, slug: slug, registrations_enabled: true, component: component, short_description: short_description }
+  let(:conference) { create :conference, organization: organization, slug: slug, registrations_enabled: true, component: component, short_description: short_description }
   let(:user) { create(:user, :confirmed, organization: organization) }
 
   before do
