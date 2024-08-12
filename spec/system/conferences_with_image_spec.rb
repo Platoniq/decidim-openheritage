8d1dbba858d3593a591063b8ffcd1cd2d8d567e5
Harrison Hassig <harrisonhassig@gmail.com>
2024-08-12 13:07:58 +0200

    Fix conference view to allow images from admin and spec file (#110)
    * Fix conference view to allow images from admin and spec file
    
    * fix quotations to satisfy rspec
    
    * Fix rspec require in factories
    
    * visit_component has component to visit
    
    * Create short_description to host the img tag
    
    * include manifest name as conferences
    
    * :conferences as manifest_name
    
    * Fix some problems in the spec
    
    * WIP: Remove img
    
    * Fix test
    
    ---------
    
    Co-authored-by: Francisco Bolívar <francisco.bolivar@nazaries.com>

---
+++ spec/system/conferences_with_image_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Conference with images", type: :system do
+  let(:organization) { create :organization }
+  let(:short_description) { { en: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p><img src=\"/favicon.ico\">" } }
+  let!(:conference) { create :conference, organization: organization, short_description: short_description }
+
+  before do
+    switch_to_host(organization.host)
+    page.visit "/conferences/#{conference.slug}"
+  end
+
+  it "shows the image" do
+    within "#content" do
+      expect(page).to have_xpath("//img[contains(@src,'favicon.ico')]")
+    end
+  end
+end
