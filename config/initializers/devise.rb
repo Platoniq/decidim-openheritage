088f0e499bb4722b452de22dfdb3ae0320dc9a5c
Alejandro Rueda López <alejandro.rueda@nazaries.com>
2025-04-16 13:19:15 +0200

    Change invitations valid time to 3 weeks (#116)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -5,4 +5,10 @@ Devise.setup do |config|
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
   config.timeout_in = 2.hours
+
+  # ==> Configuration for :invitable
+  # The period the generated invitation token is valid, after
+  # this period, the invited resource will not be able to accept the invitation.
+  # When invite_for is 0 (the default), the invitation will not expire.
+  config.invite_for = 3.weeks
 end
