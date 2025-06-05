1af97c9907761d66ee0ddf98d59fe4beb7ba4d51
Harrison Hassig <harrisonhassig@gmail.com>
2025-06-05 07:21:50 +0200

    Add show statistics superspace db files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_03_24_105356) do
+ActiveRecord::Schema.define(version: 2025_06_05_052124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
@@ -1908,6 +1908,7 @@ ActiveRecord::Schema.define(version: 2025_03_24_105356) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.jsonb "description"
+    t.boolean "show_statistics"
     t.index ["decidim_organization_id"], name: "index_decidim_superspaces_superspaces_on_decidim_organization"
   end
 
